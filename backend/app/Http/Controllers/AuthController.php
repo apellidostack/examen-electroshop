333245bd7a4c328010a3e1fe0bc92855c85116a5
apellidostack <apellidojoan4@gmail.com>
2025-08-02 16:40:29 -0400

    login completado

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request){
+        $request ->validate([
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password,$user->password)){
+            return response()->json(['message'=>'Credenciales incorrectas'],401);
+        }
+        $token = $user->createToken('api_token')->plainTextToken;
+        return response()->json([
+            'message'=>'Login exitoso',
+            'token'=>$token,
+            'user'=>$user
+        ]);
+    }
+
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|string|email|max:255|unique:users',
+            'password'=>'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+        return response()->json([
+            'message'=>'usuario registrado correctamente',
+            'token'=>$token,
+            'user'=>$user,
+        ],201);
+    }
+}
